e2f50ae2178cf7e49d96c086f03caf97bbfbecbd
Snagrap <karlkarilaid04@gmail.com>
2021-10-18 11:24:32 +0300

    getted all tasks from LS and showed at UI

--- classes/UI.js
+++ classes/UI.js
@@ -33,4 +33,13 @@ class UI{
 			tasks.removeChild(tasks.firstChild);
 		}
 	}
+	getTasks(tasks){
+		for(let i = 0, i < tasks.length; i++){
+			const li = this.addUIelement("li", "collection-item", tasks[i].name)
+			const link = this.addUIelement("a", "secondary-content", "X", {"href":"#"});
+			li.appendChild(link);
+			const list=document.querySelector("ul");
+			list.appendChild(li);
+		}
+	}
 }
